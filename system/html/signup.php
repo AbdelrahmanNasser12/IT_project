67ce7f8b50c8e05a0a543a9c90996c8d8b380828
AbdelrahmanNasser12 <45365646+AbdelrahmanNasser12@users.noreply.github.com>
2018-12-25 18:18:08 +0200

    Add files via upload

---
+++ system/html/signup.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="../css/style2.css">
+	<title>Sign up</title>
+</head>
+<body>
+	<div class="wrap">
+		<h2>Sign up</h2>
+		<form>
+			<input type="text" name="" placeholder="First name">
+			<input type="text" name="" placeholder="Last name">
+			<input type="text" name="" placeholder="User name">
+			<input type="text" name="" placeholder="Email">
+			<input type="Password" name="" placeholder="Password">
+			<input type="submit" name="" value="Sign up">
+
+		</form>
+	</div>
+</body>
+</html>
