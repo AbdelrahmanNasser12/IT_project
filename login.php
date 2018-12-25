ea02ff8ab9a6a1745e033c89f5b49a6d27a5f105
AbdelrahmanNasser12 <45365646+AbdelrahmanNasser12@users.noreply.github.com>
2018-12-25 15:33:54 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Log in</title>
+	<link rel="stylesheet" type="text/css" href="style1.css">
+</head>
+<body>
+	<div class="loginbox">
+		<img src="User_Avatar_2.png" class="avatar">
+			<h1>Login</h1>
+			<form>
+				<p>Username</p>
+				<input type="text" name="" placeholder="Enter Username">
+				<p>Password</p>
+				<input type="Password" name="" placeholder="Enter Password">
+			</br>
+				<input type="submit" name="" value="Login">
+			</form>
+	</div>
+</body>
+</html>
